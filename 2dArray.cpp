b81819729f98194b9cfaa984f09a9ed29b492235
monk <ns708090100@gmail.com>
2026-02-14 23:53:55 +0530

    2d array

---
+++ 2dArray.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  const int ROWS = 3;
+  const int COLS = 3;
+
+  int arr[ROWS][COLS];
+
+  // input
+  for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLS; j++) {
+      cout << "Enter element [" << i << "][" << j << "]: ";
+      cin >> arr[i][j];
+    }
+  }
+
+  cout << "\nMatrix:\n";
+
+  // output
+  for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLS; j++) {
+      cout << arr[i][j] << " ";
+    }
+    cout << endl;
+  }
+}
